eb3ac0798e1ffca9f97b2e47071ef1438f3def8e
Ruslan Kosenkov <r.kosenkov@spb.mcart.ru>
2022-02-01 18:14:42 +0300

    New Build

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  trailingComma: 'none',
-  singleQuote: true,
-  semi: false,
-  endOfLine: 'auto',
-}
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
-  <div
-    class="container mx-auto flex flex-col items-center bg-gray-100 p-4"
-    id="app"
-  >
+  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div class="container">
-      <div class="w-full my-4"></div>
       <section>
         <div class="flex">
           <div class="max-w-xs">
@@ -13,51 +9,46 @@
             >
             <div class="mt-1 relative rounded-md shadow-md">
               <input
-                @keydown.enter="add"
                 v-model="ticker"
+                v-on:keydown.enter="add"
                 type="text"
                 name="wallet"
                 id="wallet"
-                class="
-                  block
-                  w-full
-                  pr-10
-                  border-gray-300
-                  text-gray-900
-                  focus:outline-none focus:ring-gray-500 focus:border-gray-500
-                  sm:text-sm
-                  rounded-md
-                "
+                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE"
               />
             </div>
+            <div
+              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+            >
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                BTC
+              </span>
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                DOGE
+              </span>
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                BCH
+              </span>
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                CHD
+              </span>
+            </div>
+            <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
           </div>
         </div>
         <button
           @click="add"
           type="button"
-          class="
-            my-4
-            inline-flex
-            items-center
-            py-2
-            px-4
-            border border-transparent
-            shadow-sm
-            text-sm
-            leading-4
-            font-medium
-            rounded-full
-            text-white
-            bg-gray-600
-            hover:bg-gray-700
-            transition-colors
-            duration-300
-            focus:outline-none
-            focus:ring-2
-            focus:ring-offset-2
-            focus:ring-gray-500
-          "
+          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
           <svg
             class="-ml-0.5 mr-2 h-6 w-6"
@@ -77,90 +68,15 @@
 
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
-        <div>
-          <button
-            type="button"
-            class="
-              mx-2
-              my-4
-              inline-flex
-              items-center
-              py-2
-              px-4
-              border border-transparent
-              shadow-sm
-              text-sm
-              leading-4
-              font-medium
-              rounded-full
-              text-white
-              bg-gray-600
-              hover:bg-gray-700
-              transition-colors
-              duration-300
-              focus:outline-none
-              focus:ring-2
-              focus:ring-offset-2
-              focus:ring-gray-500
-            "
-            v-if="page > 1"
-            @click="page = page - 1"
-          >
-            Назад
-          </button>
-          <button
-            type="button"
-            class="
-              mx-2
-              my-4
-              inline-flex
-              items-center
-              py-2
-              px-4
-              border border-transparent
-              shadow-sm
-              text-sm
-              leading-4
-              font-medium
-              rounded-full
-              text-white
-              bg-gray-600
-              hover:bg-gray-700
-              transition-colors
-              duration-300
-              focus:outline-none
-              focus:ring-2
-              focus:ring-offset-2
-              focus:ring-gray-500
-            "
-            v-if="hasNextPage"
-            @click="page = page + 1"
-          >
-            Вперед
-          </button>
-          <div>Фильтр: <input v-model="filter" type="text" /></div>
-        </div>
-        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="(t, idx) in paginatedTickers"
-            :key="idx"
-            @click="select(t)"
-            :class="{
-              'border-4 border-purple-800': selectedTicker === t
-            }"
-            class="
-              bg-white
-              overflow-hidden
-              shadow
-              rounded-lg
-              border-transparent border-solid border-4
-              cursor-pointer
-            "
+            v-for="t in tickers"
+            :key="t.name"
+            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }} - USD
+                {{ t.name }}
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
                 {{ t.price }}
@@ -168,22 +84,8 @@
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-              @click.stop="handleDelete(t)"
-              class="
-                flex
-                items-center
-                justify-center
-                font-medium
-                w-full
-                bg-gray-100
-                px-4
-                py-4
-                sm:px-6
-                text-md text-gray-500
-                hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
-                transition-all
-                focus:outline-none
-              "
+              @click="handleDelete(t)"
+              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
                 class="h-5 w-5"
@@ -204,23 +106,17 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedTicker" class="relative">
+      <section class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
+          VUE - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div
-            v-for="(bar, idx) in normalizedGraph"
-            :key="idx"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
+          <div class="bg-purple-800 border w-10 h-24"></div>
+          <div class="bg-purple-800 border w-10 h-32"></div>
+          <div class="bg-purple-800 border w-10 h-48"></div>
+          <div class="bg-purple-800 border w-10 h-16"></div>
         </div>
-        <button
-          @click="selectedTicker = null"
-          type="button"
-          class="absolute top-0 right-0"
-        >
+        <button type="button" class="absolute top-0 right-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -249,174 +145,34 @@
 </template>
 
 <script>
-// [x] 6) Наличие в состоянии зависимых данных / Критичность: 5+
-// [] 4) Запросы напрямую внутри компонента / Критичность: 5
-// [] 2) При удалении остается подписка на загрузку тикера / Критичность: 5
-// [x] 8) При удалении тикера, не удаляется из localeStorage / Критичность: 4
-// [] 3) Количество запросов в API / Критичность: 4
-// [] 5) Обработка ошибок api / Критичность: 4
-// [x] 1) Одинаковый код в watch / Критичность: 3
-// [] 9) localStorage и анонимные вкладки / Критичность: 3
-// [] 7) График ужасно выглядит, если много цен / Критичность: 2
-// [] 10) Магические строки ичисла (url, 5000 мс задержки, ключ api, количество тикеров на странице) / Критичность: 1
-// [x] 11) При переходе на вторую страницу добавляет "1" к page и получается page = 11
-
-// [x] График сломан, если везде одинаковые значения
-// [x] При удалении тикера остается выбор графика
-
 export default {
-  name: 'App',
-
+  name: "App",
   data() {
     return {
-      ticker: '',
-      filter: '',
-      tickers: [],
-      selectedTicker: null,
-      graph: [],
-      page: 1
-    }
+      ticker: "default",
+      tickers: [
+        { name: "DEMO1", price: "100" },
+        { name: "DEMO2", price: "200" },
+        { name: "DEMO3", price: "500" },
+      ],
+    };
   },
-
-  created() {
-    const windowData = Object.fromEntries(
-      new URL(window.location).searchParams.entries()
-    )
-
-    if (windowData.filter) {
-      this.filter = windowData.filter
-    }
-
-    if (windowData.page) {
-      this.page = windowData.page
-    }
-
-    const tickersData = localStorage.getItem('cryptonomicon-list')
-
-    if (tickersData) {
-      this.tickers = JSON.parse(tickersData)
-      this.tickers.forEach((ticker) => {
-        this.subscribeToUpdates(ticker.name)
-      })
-    }
-  },
-
-  computed: {
-    startIndex() {
-      return (this.page - 1) * 6
-    },
-
-    endIndex() {
-      return this.page * 6
-    },
-
-    filteredTickers() {
-      return this.tickers.filter((ticker) => ticker.name.includes(this.filter))
-    },
-
-    paginatedTickers() {
-      return this.filteredTickers.slice(this.startIndex, this.endIndex)
-    },
-
-    hasNextPage() {
-      return this.filteredTickers.length > this.endIndex
-    },
-
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph)
-      const minValue = Math.min(...this.graph)
-
-      if (minValue === maxValue) {
-        return this.graph.map(() => 50)
-      }
-      return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      )
-    },
-
-    pageStateOptions() {
-      return {
-        filter: this.filter,
-        page: this.page
-      }
-    }
-  },
-
   methods: {
-    subscribeToUpdates(tickerName) {
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=c7ad2664a028a12457f00dc9fabf5d26fcb0043489b90e8a28602840f173243d`
-        )
-        const data = await f.json()
-        this.tickers.find((t) => t.name === tickerName).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
-        //currentTicker.price = data.USD //TODO Реактивно не обновляется
-
-        if (this.selectedTicker?.name === tickerName) {
-          this.graph.push(data.USD)
-        }
-      }, 3000)
-      this.ticker = ''
-    },
-
     add() {
-      const currentTicker = {
+      const newTicker = {
         name: this.ticker,
-        price: '-'
-      }
+        price: "1000",
+      };
 
-      if (this.ticker !== '') {
-        this.tickers = [...this.tickers, currentTicker]
-        this.filter = ''
-
-        localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
-
-        this.subscribeToUpdates(currentTicker.name)
-      }
-    },
-
-    select(ticker) {
-      this.selectedTicker = ticker
+      this.tickers.push(newTicker);
+      this.ticker = "";
     },
 
     handleDelete(tickerToRemove) {
-      this.tickers = this.tickers.filter((t) => t !== tickerToRemove)
-      if (this.selectedTicker === tickerToRemove) {
-        this.selectedTicker = null
-      }
-    }
+      this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+    },
   },
-
-  watch: {
-    selectedTicker() {
-      this.graph = []
-    },
-
-    tickers(newValue, oldValue) {
-      // Почему не сработал watch при добавлении тикера?
-      console.log(newValue.length, oldValue.length)
-
-      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
-    },
-
-    paginatedTickers() {
-      if (this.paginatedTickers.length === 0 && this.page > 1) {
-        this.page -= 1
-      }
-    },
-
-    filter() {
-      this.page = 1
-    },
-
-    pageStateOptions(value) {
-      window.history.pushState(
-        null,
-        document.title,
-        `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
-      )
-    }
-  }
-}
+};
 </script>
+
+<style src="./app.css"></style>
